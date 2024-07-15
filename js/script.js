6699d8ead85248902a0fefdcc810d6af96f3051d
Maxi Gimenez <maximilianogimenez631@outlook.com>
2024-07-15 19:41:56 -0300

    arreglos de errores y sacamos el login

--- js/script.js
+++ js/script.js
@@ -4,18 +4,3 @@ function openNav(){
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
 }
-const pass = document.getElementById("password-vernover"),
-      icon = document.querySelector(".bx-pass");
-
-icon.addEventListener("click", e =>{
-    if(pass.type === "password"){
-        pass.type = "text";
-        icon.classList.remove(`fa-eye-slash`);
-        icon.classList.add(`fa-eye`);
-    }
-    else{
-        pass.type = "password"
-        icon.classList.add(`fa-eye-slash`);
-        icon.classList.remove(`fa-eye`);
-    }
-})
